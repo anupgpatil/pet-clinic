4592b98ba400a2188bde3c4e5b9a2f7995cfa282
Anup <anupgpatil@gmail.com>
2020-04-18 15:30:48 -0700

    Adding Id to Models . Closes #10.

--- pet-clinic-data/src/main/java/anup/learnspring/model/Person.java
+++ pet-clinic-data/src/main/java/anup/learnspring/model/Person.java
@@ -1,6 +1,6 @@
 package anup.learnspring.model;
 
-public class Person {
+public class Person extends BaseEntity{
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/anup/learnspring/model/PetType.java
+++ pet-clinic-data/src/main/java/anup/learnspring/model/PetType.java
@@ -1,6 +1,6 @@
 package anup.learnspring.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
 
     private String name;
 
