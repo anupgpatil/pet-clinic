e56caa517dbc1d9566701413a5a8a89120b0550b
Anup <anupgpatil@gmail.com>
2020-04-19 15:20:49 -0700

    Refactoring Services to Common Interface. Closes #11.

--- pet-clinic-data/src/main/java/anup/learnspring/services/PetService.java
+++ pet-clinic-data/src/main/java/anup/learnspring/services/PetService.java
@@ -2,13 +2,6 @@ package anup.learnspring.services;
 
 import anup.learnspring.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long>{
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
