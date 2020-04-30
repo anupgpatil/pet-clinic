d54e35f3a6d0779befbc5011bf0a35993f8d564f
Anup <anupgpatil@gmail.com>
2020-04-29 21:46:58 -0700

    Map Implementation. Closes #23.

--- pet-clinic-web/src/main/java/anup/learnspring/bootstrap/InitialDataLoad.java
+++ pet-clinic-web/src/main/java/anup/learnspring/bootstrap/InitialDataLoad.java
@@ -23,14 +23,14 @@ public class InitialDataLoad implements CommandLineRunner {
 
         Owner owner = new Owner();
         owner.setFirstName("Owner 1");
-        owner.setId(1L);
+        //owner.setId(1L);
         owner.setLastName("Owner LName");
 
         ownerService.save(owner);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Owner 2");
-        owner2.setId(2L);
+        //owner2.setId(2L);
         owner2.setLastName("Owner2 LName");
 
         ownerService.save(owner2);
@@ -38,14 +38,14 @@ public class InitialDataLoad implements CommandLineRunner {
         System.out.println("Loaded Data for Owners ");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
+        //vet1.setId(1L);
         vet1.setFirstName("Vet FName");
         vet1.setLastName("Vet1 LName");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
+        //vet2.setId(2L);
         vet2.setFirstName("Vet2 FName");
         vet2.setLastName("Vet2 LName");
 
