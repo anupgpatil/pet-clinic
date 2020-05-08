0146a74fcf0d06fa5eef9c0c6735db6fce3f6716
Anup <anupgpatil@gmail.com>
2020-05-07 22:24:46 -0700

    Implement Spring Data JPA for Owners . Closes #40.

--- pet-clinic-data/src/main/java/anup/learnspring/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/anup/learnspring/repositories/OwnerRepository.java
@@ -4,4 +4,6 @@ import anup.learnspring.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    Owner findByLastName(String lastName);
 }
