2bb3b20149c9912697e14f1c783060ed3098e7d1
Anup <anupgpatil@gmail.com>
2020-05-08 11:48:03 -0700

    Implement Spring Data JPA for Vets . Closes #41.

--- pet-clinic-data/src/main/java/anup/learnspring/services/springdatajpa/OwnerSDJpaService.java
+++ pet-clinic-data/src/main/java/anup/learnspring/services/springdatajpa/OwnerSDJpaService.java
@@ -13,13 +13,13 @@ import java.util.Set;
 
 @Service
 @Profile("springdatajpa")
-public class OwnerSDJPAService implements OwnerService {
+public class OwnerSDJpaService implements OwnerService {
 
     private final OwnerRepository ownerRepository;
     private final PetRepository petRepository;
     private final PetTypeRepository petTypeRepository;
 
-    public OwnerSDJPAService(OwnerRepository ownerRepository, PetRepository petRepository,
+    public OwnerSDJpaService(OwnerRepository ownerRepository, PetRepository petRepository,
                              PetTypeRepository petTypeRepository) {
         this.ownerRepository = ownerRepository;
         this.petRepository = petRepository;
