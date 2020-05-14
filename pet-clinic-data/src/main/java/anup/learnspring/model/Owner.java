da4bbe5c5b9926994ed41cec56ea1cd34bec25e1
Anup <anupgpatil@gmail.com>
2020-05-13 22:42:04 -0700

    Adding Project Lombok . Closes #51.

--- pet-clinic-data/src/main/java/anup/learnspring/model/Owner.java
+++ pet-clinic-data/src/main/java/anup/learnspring/model/Owner.java
@@ -1,11 +1,18 @@
 package anup.learnspring.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 /**/
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Table(name="owners")
 public class Owner extends Person {
     @Column(name="address")
@@ -19,36 +26,4 @@ public class Owner extends Person {
 
     @OneToMany(cascade = CascadeType.ALL , mappedBy = "owner")
     Set<Pet> pets = new HashSet<>();
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getTelephone() {
-        return telephone;
-    }
-
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
-    }
-
-    public Set<Pet> getPets() {
-        return pets;
-    }
-
-    public void setPets(Set<Pet> pets) {
-        this.pets = pets;
-    }
 }
--- pet-clinic-data/src/main/java/anup/learnspring/model/Person.java
+++ pet-clinic-data/src/main/java/anup/learnspring/model/Person.java
@@ -1,9 +1,14 @@
 package anup.learnspring.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 
 @MappedSuperclass
+@Getter
+@Setter
 public class Person extends BaseEntity{
 
     @Column(name="first_name")
@@ -11,20 +16,4 @@ public class Person extends BaseEntity{
 
     @Column(name="last_name")
     private String lastName;
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
 }
